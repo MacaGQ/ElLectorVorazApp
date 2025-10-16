56926c9a3618f1d693218eb69ab8560715927906
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-10-16 20:27:53 -0300

    se suman pantallas de registros libros, revistas y articulos de libreria

---
+++ app/src/main/java/com/example/ellectorvoraz/P18_PantallaDeRegistroArticulosLibreria.kt
@@ -0,0 +1,7 @@
+package com.example.ellectorvoraz
+
+import android.app.Activity
+
+class P18_PantallaDeRegistroArticulosLibreria : Activity() {
+
+}
---
+++ app/src/main/java/com/example/ellectorvoraz/P20_PantallaRegistroArticuloCorrecto.kt
@@ -0,0 +1,7 @@
+package com.example.ellectorvoraz
+
+import android.app.Activity
+
+class P20_PantallaRegistroArticuloCorrecto : Activity() {
+
+}
