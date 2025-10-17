439aa042f3d1a94a734a1c6bc6f687f6cd80c272
Macarena Garcia Quintans <macarenadgq@gmail.com>
2025-10-16 22:54:17 -0300

    Agregada interaccion en Sistema General

---
+++ app/src/main/java/com/example/ellectorvoraz/P75_PantallaDeGestion.kt
@@ -0,0 +1,11 @@
+package com.example.ellectorvoraz
+
+import android.app.Activity
+import android.os.Bundle
+
+class P75_PantallaDeGestion : Activity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_p75_pantalla_de_gestion)
+    }
+}
--- app/src/main/java/com/example/ellectorvoraz/P7_PantallaSistemaGeneral.kt
+++ app/src/main/java/com/example/ellectorvoraz/P7_PantallaSistemaGeneral.kt
@@ -1,7 +1,24 @@
 package com.example.ellectorvoraz
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
 
 class P7_PantallaSistemaGeneral : Activity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_p7_pantalla_sistema_general)
 
+        val btnCatalogo = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnCatalogos)
+        btnCatalogo.setOnClickListener(){
+            val intent = Intent(this, P10_PantallaDeSistemaParticular::class.java)
+            startActivity(intent)
+        }
+
+        val btnGestion = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnGestion)
+        btnGestion.setOnClickListener(){
+            val intent = Intent(this, P75_PantallaDeGestion::class.java)
+            startActivity(intent)
+        }
+    }
 }
