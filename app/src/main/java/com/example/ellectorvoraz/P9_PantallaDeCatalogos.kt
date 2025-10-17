f9ae76d6c501d4a6cb68808b2701946da14bbd6d
Macarena Garcia Quintans <macarenadgq@gmail.com>
2025-10-16 23:09:17 -0300

    Agregado view a catalogos

--- app/src/main/java/com/example/ellectorvoraz/P9_PantallaDeCatalogos.kt
+++ app/src/main/java/com/example/ellectorvoraz/P9_PantallaDeCatalogos.kt
@@ -1,7 +1,11 @@
 package com.example.ellectorvoraz
 
 import android.app.Activity
+import android.os.Bundle
 
 class P9_PantallaDeCatalogos : Activity() {
-
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_p9_pantalla_de_catalogos)
+    }
 }
