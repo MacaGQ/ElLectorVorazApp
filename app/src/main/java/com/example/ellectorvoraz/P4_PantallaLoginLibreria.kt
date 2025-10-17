1e1951f98884622c303188eb24e4491a8062daa2
Macarena Garcia Quintans <macarenadgq@gmail.com>
2025-10-16 22:14:10 -0300

    Correccion ingreso en Login Libreria

--- app/src/main/java/com/example/ellectorvoraz/P4_PantallaLoginLibreria.kt
+++ app/src/main/java/com/example/ellectorvoraz/P4_PantallaLoginLibreria.kt
@@ -1,7 +1,30 @@
 package com.example.ellectorvoraz
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class P4_PantallaLoginLibreria : Activity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_p4_pantalla_login_libreria)
+        val flechaVolver = findViewById<ImageView>(R.id.flechaVolver)
+        flechaVolver.setOnClickListener {
+            finish()
+        }
 
+        val btnIngresar = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener {
+            val intent = Intent(this, P7_PantallaSistemaGeneral::class.java)
+            startActivity(intent)
+        }
+
+        val btnRegistro = findViewById<TextView>(R.id.textView_Pregunta_registro)
+        btnRegistro.setOnClickListener {
+            val intent = Intent(this, P5_PantallaRegistroLibreria::class.java)
+            startActivity(intent)
+        }
+    }
 }
