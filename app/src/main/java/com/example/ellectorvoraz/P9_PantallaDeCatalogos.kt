21eb3360257015527ae1321960c2e2e616217a13
Macarena Garcia Quintans <macarenadgq@gmail.com>
2025-10-16 23:23:02 -0300

    Agregado boton Volver en SisGral, Catalogos y Gestion

--- app/src/main/java/com/example/ellectorvoraz/P9_PantallaDeCatalogos.kt
+++ app/src/main/java/com/example/ellectorvoraz/P9_PantallaDeCatalogos.kt
@@ -2,10 +2,16 @@ package com.example.ellectorvoraz
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ImageView
 
 class P9_PantallaDeCatalogos : Activity() {
     override fun onCreate(saveInstanceState: Bundle?) {
         super.onCreate(saveInstanceState)
         setContentView(R.layout.activity_p9_pantalla_de_catalogos)
+
+        val btnVolver = findViewById<ImageView>(R.id.flechaVolver)
+        btnVolver.setOnClickListener(){
+            finish()
+        }
     }
 }
